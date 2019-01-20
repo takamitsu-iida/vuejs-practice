90c25baf2d64662efede2414a22472020bbb7746
takamitsu-iida <takamitsu.iida@gmail.com>
2019-01-20 19:03:51 +0900

    [ci skip]

--- README.md
+++ README.md
@@ -1,7 +1,66 @@
 # vue.jsの練習
 
+チュートリアルの写経です。
+
+<https://jp.vuejs.org/v2/guide/index.html>
+
 ## ライブデモ(github pages)
 
 練習１．
 
 <https://takamitsu-iida.github.io/vuejs-practice/index.1.html>
+
+## 省略記法
+
+### v-bind
+
+```html
+<!-- 完全な構文 -->
+<a v-bind:href="url"> ... </a>
+
+<!-- 省略記法 -->
+<a :href="url"> ... </a>
+```
+
+### v-on
+
+```html
+<!-- 完全な構文 -->
+<a v-on:click="doSomething"> ... </a>
+
+<!-- 省略記法 -->
+<a @click="doSomething"> ... </a>
+```
+
+## 複雑なロジックには算出プロパティを利用すべき
+
+算出プロパティの代わりに、同じような関数をメソッドとして定義することも可能です。
+最終的には、2つのアプローチは完全に同じ結果になります。
+しかしながら、算出プロパティは依存関係にもとづきキャッシュされるという違いがあります。
+算出プロパティは、それが依存するものが更新されたときにだけ再評価されます。
+
+飯田メモ： methodsではなくcomputedを使え、ということ。
+
+```html
+<div id="example">
+  <p>Original message: "{{ message }}"</p>
+  <p>Computed reversed message: "{{ reversedMessage }}"</p>
+</div>
+```
+
+```js
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 算出 getter 関数
+    reversedMessage: function () {
+      // `this` は vm インスタンスを指します
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+```
+
--- static/site/js/iida.vuejs.1.js
+++ static/site/js/iida.vuejs.1.js
@@ -1,16 +1,78 @@
 /* global Vue, iida */
+/* eslint no-unused-vars: 0 */
 
 (function() {
 
   // iida.の下にvue.jsを走らせる関数を作成する
   iida.vuejs = function() {
 
-    new Vue({
-      el: '#app',
+    var app1 = new Vue(
+      {
+        el: '#app',
+        data: {
+          message: 'Hello Vue.js!'
+        }
+      }
+    );
+
+    var app2 = new Vue(
+      {
+        el: '#app-2',
+        data: {
+          message: 'You loaded this page on ' + new Date().toLocaleString()
+        }
+      }
+    );
+
+
+    var app3 = new Vue({
+      el: '#app-3',
       data: {
-        message: 'Hello Vue.js!'
+        seen: true
       }
     });
+
+    var app4 = new Vue({
+      el: '#app-4',
+      data: {
+        todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ]
+      }
+    });
+
+    var app5 = new Vue({
+      el: '#app-5',
+      data: {
+        message: 'Hello Vue.js!'
+      },
+      // <button v-on:click="reverseMessage">
+      methods: {
+        reverseMessage: function () {
+          this.message = this.message.split('').reverse().join('');
+        }
+      }
+    });
+
+
+    Vue.component('todo-item', {
+      props: ['todo'],
+      template: '<li>{{ todo.text }}</li>'
+    });
+    // <todo-item v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id"></todo-item>
+    var app7 = new Vue({
+      el: '#app-7',
+      data: {
+        groceryList: [
+          { id: 0, text: 'Vegetables' },
+          { id: 1, text: 'Cheese' },
+          { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+      }
+    });
+
 
   };
   //
